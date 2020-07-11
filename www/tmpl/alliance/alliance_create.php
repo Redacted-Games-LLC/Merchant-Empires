3807680f627a319ddb51f0b5731dedf47433a9a1
Desmond Kung <desmondkung@users.noreply.github.com>
2020-07-11 16:38:04 +0800

    Removed label.ship_form_label as undefined

--- www/tmpl/alliance/alliance_create.php
+++ www/tmpl/alliance/alliance_create.php
@@ -46,7 +46,7 @@
 	</div>
 	<div class="docs_text">
 		<form action="handler.php" method="post">
-			<label class="ship_form_label" for="alliance_name">Alliance Name:</label>
+			<label for="alliance_name">Alliance Name:</label>
 			<input class="ship_form_input" type="text" name="name" id="alliance_name" maxlength="24" size="30" />
 
 			<script type="text/javascript">drawButton('create', 'create', 'validate_create()');</script>
