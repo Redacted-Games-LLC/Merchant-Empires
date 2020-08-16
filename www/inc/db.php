169075f62807d971f99171d1a15d5e1d86b174f0
Desmond Kung <desmondkung@users.noreply.github.com>
2020-08-16 20:44:01 +0800

    Initial config of SSL connection to DB

--- www/inc/db.php
+++ www/inc/db.php
@@ -40,12 +40,31 @@
 		function __construct($user = false) {
 
 			$db = null;
+			
+			// Un-comment for SSL connection to DB
+			//$db = mysqli_init();
+			//if (!$db) {
+			//	die("mysqli_init failed");
+			//}
+			//$db->ssl_set(NULL, NULL, SQL_SSL_CA_CERT, NULL, NULL);
 
 			if ($user) {
 				$db = @(new MySQLi(USER_DB_HOST, USER_DB_USER, USER_DB_PASS, USER_DB_NAME));
+				// Comment line above and un-comment lines below for SSL connection to DB
+				//if(!$db->real_connect(USER_DB_HOST, USER_DB_USER, USER_DB_PASS, USER_DB_NAME, USER_DB_PORT, MYSQLI_CLIENT_SSL)) {
+				//	error_log(__FILE__ . '::' . __LINE__ . 'User database connect error: (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+				//	header('Location: login.php?rc=1165');
+				//	die();
+				//}
 			}
 			else {
 				$db = @(new MySQLi(DB_HOST, DB_USER, DB_PASS, DB_NAME));
+				// Comment line above and un-comment lines below for SSL connection to DB
+				//if(!$db->real_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT, MYSQLI_CLIENT_SSL)) {
+				//	error_log(__FILE__ . '::' . __LINE__ . 'Game database connect error: (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+				//	header('Location: login.php?rc=1165');
+				//	die();
+				//}
 			}
 			
 			if ($db->connect_errno) {
